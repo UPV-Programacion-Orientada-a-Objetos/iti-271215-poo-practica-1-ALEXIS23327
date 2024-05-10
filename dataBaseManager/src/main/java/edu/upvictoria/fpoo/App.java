8687d70d21ab8d312311aedc29d0cbdcc49fa842
ALEXIS23327 <128847011+ALEXIS23327@users.noreply.github.com>
2024-05-09 21:15:45 -0600

    command DELETE

--- dataBaseManager/src/main/java/edu/upvictoria/fpoo/App.java
+++ dataBaseManager/src/main/java/edu/upvictoria/fpoo/App.java
@@ -6,7 +6,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-
 public class App {
     public static void main(String[] args) throws IOException {
         BufferedReader lector = new BufferedReader(new InputStreamReader(System.in));
