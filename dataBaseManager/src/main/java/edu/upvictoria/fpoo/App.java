31d846ba85eba8a8b73c313b52b1304c22b7293d
ALEXIS23327 <128847011+ALEXIS23327@users.noreply.github.com>
2024-05-09 19:15:47 -0600

    App.java

--- dataBaseManager/src/main/java/edu/upvictoria/fpoo/App.java
+++ dataBaseManager/src/main/java/edu/upvictoria/fpoo/App.java
@@ -1,1 +1,40 @@
+package edu.upvictoria.fpoo;
 
+import edu.upvictoria.fpoo.Exceptions.ErrorSintaxis;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class App {
+    public static void main(String[] args) throws IOException {
+        BufferedReader lector = new BufferedReader(new InputStreamReader(System.in));
+        String op;
+        String use;
+        boolean continuar = true; // Variable para controlar la salida del bucle
+        System.out.println("ESCRIBE [exit] para salir.");
+
+        do {
+            System.out.print("BASE DE DATOS:");
+            use = String.valueOf(lector.readLine());
+            if (!use.startsWith("USE $PATH$;")) {
+                System.out.println("INVALIDO");
+            }
+        } while ((!use.startsWith("USE $PATH$;")));
+
+        do {
+            try {
+                Sentencias.menu(lector);
+                op = lector.readLine();
+                if (op.equalsIgnoreCase("exit;")){
+                    continuar = false; // Establecer la variable a false para salir del bucle
+                }
+            } catch (ErrorSintaxis e) {
+                System.out.println("Error de sintaxis: " + e.getMessage());
+            } catch (Exception e) {
+                System.out.println("Ocurrió un error: " + e.getMessage());
+            }
+        } while (continuar); // La condición de salida ahora depende de la variable 'continuar'
+    }
+}
